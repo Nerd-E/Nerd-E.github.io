22201b0b12280602053d75a6f15662fe5db0891a
Nerd-E <42470662+Nerd-E@users.noreply.github.com>
2023-12-01 19:12:35 -0500

    FULL refactor, using this as a basis for adding new stuff
    CREDIT https://youtu.be/KtYby2QN0kQ?si=eHFxTUPTITmuQk5x

--- js/main.js
+++ js/main.js
@@ -1,172 +1,190 @@
-var scanlines = $('.scanlines');
-var tv = $('.tv');
-function exit() {
-    $('.tv').addClass('collapse');
-    term.disable();
-}
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
 
-// ref: https://stackoverflow.com/q/67322922/387194
-var __EVAL = (s) => eval(`void (__EVAL = ${__EVAL}); ${s}`);
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
 
-var term = $('#terminal').terminal(function(command, term) {
-    var cmd = $.terminal.parse_command(command);
-    if (cmd.name === 'exit') {
-        exit();
-    } else if (cmd.name === 'echo') {
-        term.echo(cmd.rest);
-    } else if (command !== '') {
-        try {
-            var result = __EVAL(command);
-            if (result && result instanceof $.fn.init) {
-                term.echo('<#jQuery>');
-            } else if (result && typeof result === 'object') {
-                tree(result);
-            } else if (result !== undefined) {
-                term.echo(new String(result));
-            }
-        } catch(e) {
-            term.error(new String(e));
-        }
-    }
-}, {
-    name: 'js_demo',
-    onResize: set_size,
-    exit: false,
-    // detect iframe codepen preview
-    enabled: $('body').attr('onload') === undefined,
-    onInit: function() {
-        set_size();
-        this.echo('Type [[b;#fff;]exit] to see turn off animation.');
-        this.echo('Type and execute [[b;#fff;]grab()] function to get the scre' +
-                  'enshot from your camera');
-        this.echo('Type [[b;#fff;]camera()] to get video and [[b;#fff;]pause()]/[[b;#fff;]play()] to stop/play');
-    },
-    onClear: function() {
-        console.log(this.find('video').length);
-        this.find('video').map(function() {
-            console.log(this.src);
-            return this.src;
-        });
-    },
-    prompt: 'js> '
-});
-// for codepen preview
-if (!term.enabled()) {
-    term.find('.cursor').addClass('blink');
-}
-function set_size() {
-    // for window height of 170 it should be 2s
-    var height = $(window).height();
-    var width = $(window).width()
-    var time = (height * 2) / 170;
-    scanlines[0].style.setProperty("--time", time);
-    tv[0].style.setProperty("--width", width);
-    tv[0].style.setProperty("--height", height);
-}
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
 
-function tree(obj) {
-    term.echo(treeify.asTree(obj, true, true));
-}
-var constraints = {
-    audio: false,
-    video: {
-        width: { ideal: 1280 },
-        height: { ideal: 1024 },
-        facingMode: "environment"
-    }
-};
-var acceptStream = (function() {
-    return 'srcObject' in document.createElement('video');
-})();
-function camera() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        term.pause();
-        var media = navigator.mediaDevices.getUserMedia(constraints);
-        media.then(function(mediaStream) {
-            term.resume();
-            var stream;
-            if (!acceptStream) {
-                stream = window.URL.createObjectURL(mediaStream);
-            } else {
-                stream = mediaStream;
-            }
-            term.echo('<video data-play="true" class="self"></video>', {
-                raw: true,
-                onClear: function() {
-                    if (!acceptStream) {
-                        URL.revokeObjectURL(stream);
-                    }
-                    mediaStream.getTracks().forEach(track => track.stop());
-                },
-                finalize: function(div) {
-                    var video = div.find('video');
-                    if (!video.length) {
-                        return;
-                    }
-                    if (acceptStream) {
-                        video[0].srcObject = stream;
-                    } else {
-                        video[0].src = stream;
-                    }
-                    if (video.data('play')) {
-                        video[0].play();
-                    }
-                }
-            });
-        });
-    }
-}
-var play = function() {
-    var video = term.find('video').slice(-1);
-    if (video.length) {
-        video[0].play();
-    }
-}
-function pause() {
-    term.find('video').each(function() {
-        this.pause(); 
-    });
-}
+window.addEventListener("keyup", enterKey);
 
-function grab() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        term.pause();
-        var media = navigator.mediaDevices.getUserMedia(constraints);
-        media.then(function(mediaStream) {
-            const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-            const imageCapture = new ImageCapture(mediaStreamTrack);
-            return imageCapture.takePhoto();
-        }).then(function(blob) {
-            term.echo('<img src="' + URL.createObjectURL(blob) + '" class="self"/>', {
-                raw: true,
-                finialize: function(div) {
-                    div.find('img').on('load', function() {
-                        URL.revokeObjectURL(this.src);
-                    });
-                }
-            }).resume();
-        }).catch(function(error) {
-            term.error('Device Media Error: ' + error);
-        });
+console.log(
+  "%cYou hacked my password!😠",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log("%cPassword: '" + password + "' - I wonder what it does?🤔", "color: grey");
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
     }
-}
-async function pictuteInPicture() {
-    var [video] = $('video');
-    try {
-        if (video) {
-            if (video !== document.pictureInPictureElement) {
-                await video.requestPictureInPicture();
-            } else {
-                await document.exitPictureInPicture();
-            }
-        }
-  } catch(error) {
-      term.error(error);
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("visitor@fkcodes.com:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
   }
 }
-function clear() {
-    term.clear();
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "video":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+      }, 1000); 
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "password":
+      addLine("<span class=\"inherit\"> Lol! You're joking, right? You\'re gonna have to try harder than that!😂</span>", "error", 100);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:forrest@fkcodes.com">forrest@fkcodes.com</a>...', "color2", 80);
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "youtube":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
 }
 
-github('jcubic/jquery.terminal');
-cssVars(); // ponyfill
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
